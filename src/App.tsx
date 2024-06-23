87b0740f58888d772b6a9346d9f9aea7c6c31e02
6lineir <ali.baghaeiheravi77@gmail.com>
2024-06-24 00:28:36 +0330

    Starter

---
+++ src/App.tsx
@@ -0,0 +1,138 @@
+import { useEffect, useState } from 'react';
+import './index.css';
+import { bear, coin, highVoltage, notcoin, rocket, trophy } from './images';
+
+const App = () => {
+  const preCoin = parseInt(localStorage.getItem('coin'))
+  console.log(preCoin);
+  if (localStorage.getItem('coin') === null) {
+    localStorage.setItem('coin', parseInt(0))
+  }
+  const [points, setPoints] = useState(preCoin);
+  const [energy, setEnergy] = useState(100);
+  const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
+  const pointsToAdd = 1;
+  const energyToReduce = 1;
+  const handleLevel = ()=>{
+      console.warn(points);
+      
+  }
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (energy - energyToReduce < 0) {
+      return;
+    }
+
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    setPoints(points + pointsToAdd);
+    localStorage.setItem('coin', points + pointsToAdd)
+    setEnergy(energy - energyToReduce < 0 ? 0 : energy - energyToReduce);
+    setClicks([...clicks, { id: Date.now(), x, y }]);
+  };
+
+  const handleAnimationEnd = (id: number) => {
+    setClicks((prevClicks) => prevClicks.filter(click => click.id !== id));
+  };
+
+  // useEffect hook to restore energy over time
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setEnergy((prevEnergy) => Math.min(prevEnergy + 1, 100));
+    }, 1000); // Restore 10 energy points every second
+
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, []);
+
+  return (
+    <div className="bg-gradient-main min-h-screen px-4 flex flex-col items-center text-white font-medium">
+
+      <div className="absolute inset-0 h-1/2 bg-gradient-overlay z-0"></div>
+      <div className="absolute inset-0 flex items-center justify-center z-0">
+        <div className="radial-gradient-overlay"></div>
+      </div>
+
+      <div className="w-full z-10 min-h-screen flex flex-col items-center text-white">
+
+        <div className="fixed top-0 left-0 w-full px-4 pt-8 z-10 flex flex-col items-center text-white">
+          <div className="w-full cursor-pointer">
+            <div className="bg-[#1f1f1f] text-center py-2 rounded-xl">
+              <a href="https://t.me/test" target='_blank'>
+                <p className="text-lg">Join Channel</p>
+              </a>
+            </div>
+          </div>
+          <div className="mt-10 flex items-center">
+            <img src={coin} width={40} height={40} />
+            <span className="count-point ml-2">{points.toLocaleString()}</span>
+          </div>
+          <div className="text-base mt-2 flex items-center">
+            <img src={trophy} width={24} height={24} />
+            <span className="ml-1">Gold</span>
+          </div>
+        </div>
+
+
+        <div className="fixed bottom-0 left-0 w-full px-4 pb-4 z-10">
+          <div className="w-full flex justify-between gap-2">
+            <div className="w-1/3 flex items-center justify-start max-w-32">
+              <div className="flex items-center justify-center">
+                <img src={highVoltage} width={44} height={44} alt="High Voltage" />
+                <div className="ml-2 text-left">
+                  <span className="text-white text-2xl font-bold block">{energy}</span>
+                  <span className="text-white text-large opacity-75">/ 100</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex-grow flex items-center max-w-60 text-sm">
+              <div className="w-full bg-[#fad258] py-4 rounded-2xl flex justify-around">
+                <button className="flex flex-col items-center gap-1">
+                  <img src={bear} width={24} height={24} alt="High Voltage" />
+                  <span>Frens</span>
+                </button>
+                <div className="h-[48px] w-[2px] bg-[#fddb6d]"></div>
+                <button className="flex flex-col items-center gap-1">
+                  <img src={coin} width={24} height={24} alt="High Voltage" />
+                  <span>Earn</span>
+                </button>
+                <div className="h-[48px] w-[2px] bg-[#fddb6d]"></div>
+                <button className="flex flex-col items-center gap-1">
+                  <img src={rocket} width={24} height={24} alt="High Voltage" />
+                  <span>Boosts</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full bg-[#f9c035] rounded-full mt-4">
+            <div className="bg-gradient-to-r from-[#f3c45a] to-[#fffad0] h-4 rounded-full" style={{ width: `${(energy / 100) * 100}%` }}></div>
+          </div>
+        </div>
+
+
+        <div className="flex-grow flex items-center justify-center">
+          <div className="relative mt-4 csscoin" onClick={handleClick}>
+            <img src={notcoin} width={220} height={220} alt="coin" />
+            {clicks.map((click) => (
+              <div
+                key={click.id}
+                className="absolute text-5xl font-bold opacity-0 "
+                style={{
+                  top: `${click.y - 42}px`,
+                  left: `${click.x - 28}px`,
+                  animation: `float 1s ease-out`
+                }}
+                onAnimationEnd={() => handleAnimationEnd(click.id)}
+              >
+                1
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
+    </div >
+  );
+};
+
+export default App;
